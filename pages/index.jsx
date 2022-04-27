d7c4713f702beeb36a38c1405b270c3a4d99c277
Qubism <93381881+QubismNFT@users.noreply.github.com>
2022-04-27 10:54:11 +0100

    Add files via upload

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Activity from './Activity'
+import Auctions from "./Auctions"
+import Contact from "./Contact"
+import CreateItem from "./CreateItem"
+import Listed from "./Listed"
+import Explore from "./Explore"
+import Home from "./Home"
+import ItemDetails from "./ItemDetails"
+import EditProfile from "./EditProfile"
+import Profile from "./Profile"
+
+
+export {Activity, Auctions, Contact, CreateItem, Listed, Explore, Home, ItemDetails, EditProfile, Profile}
